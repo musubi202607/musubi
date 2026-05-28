dae501312aa4bd85d78903c45a6ac6193a28300c
musubi202607 <musubi.202607@gmail.com>
2026-05-28 10:14:11 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,55 @@
+async function loadProducts() {
+
+  const response =
+    await fetch(
+      API_URL + '?mode=products'
+    );
+
+  const products =
+    await response.json();
+
+  const grid =
+    document.getElementById(
+      'productGrid'
+    );
+
+  grid.innerHTML = '';
+
+  products.forEach(product => {
+
+    grid.innerHTML += `
+
+      <div class="product-card">
+
+        <img src="${product.image}">
+
+        <div class="product-content">
+
+          <h3>
+            ${product.name}
+          </h3>
+
+          <p>
+            ${product.description}
+          </p>
+
+          <div class="price">
+            ¥${product.price}
+          </div>
+
+          <button
+            onclick="addToCart(${product.id})"
+          >
+            カートに追加
+          </button>
+
+        </div>
+
+      </div>
+
+    `;
+  });
+
+}
+
+loadProducts();
---
+++ js/config.js
@@ -0,0 +1,2 @@
+const API_URL =
+'https://script.google.com/macros/s/AKfycbxes5dE1GhMaJzW2zh-TU1zNGjd9n-YQqDwXgFEYq-N4MxjDJr-RARQUa87-rxD_u8fQg/exec';
