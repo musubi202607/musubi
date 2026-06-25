bc27032081f65304115242d1e33948bdbee86da2
musubi202607 <musubi.202607@gmail.com>
2026-06-25 13:00:24 +0900

    Add files via upload

--- js/order.js
+++ js/order.js
@@ -1,3 +1,106 @@
+async function loadOrderItems(){
+
+  const sessionId =
+    localStorage.getItem("sessionId");
+
+  const cartRes =
+    await fetch(
+      API_URL +
+      "/api/cart/get?sessionId=" +
+      sessionId
+    );
+
+  const cart =
+    await cartRes.json();
+
+  if(!cart.length){
+
+    document.getElementById(
+      "orderItems"
+    ).innerHTML =
+      "<h2>商品がありません</h2>";
+
+    return;
+
+  }
+
+  const productsRes =
+    await fetch(
+      API_URL +
+      "/api/products"
+    );
+
+  const products =
+    await productsRes.json();
+
+  let html = `
+    <table class="order-table">
+
+      <tr>
+
+        <th>商品名</th>
+        <th>数量</th>
+        <th>金額</th>
+
+      </tr>
+  `;
+
+  let total = 0;
+
+  cart.forEach(item => {
+
+    const product =
+      products.find(
+        p =>
+          Number(p.id) ===
+          Number(item.id)
+      );
+
+    if(!product) return;
+
+    const amount =
+      Number(product.price) *
+      Number(item.qty);
+
+    total += amount;
+
+    html += `
+      <tr>
+
+        <td>
+          ${product.name}
+        </td>
+
+        <td>
+          ${item.qty}
+        </td>
+
+        <td>
+          ¥${amount.toLocaleString()}
+        </td>
+
+      </tr>
+    `;
+
+  });
+
+  html += `
+    </table>
+
+    <div class="total-area">
+
+      合計：
+      ¥${total.toLocaleString()}
+
+    </div>
+  `;
+
+  document.getElementById(
+    "orderItems"
+  ).innerHTML = html;
+
+}
+
 async function sendOrder(){
 
   const sessionId =
@@ -36,8 +139,19 @@ async function sendOrder(){
     );
 
   const json =
-    await res.json();
+  await res.json();
 
-  console.log(json);
+console.log(json);
+
+if(json.success){
+
+  location.href =
+    "complete.html";
+
+}else{
+
+  alert(
+    "注文送信エラー"
+  );
 
 }
