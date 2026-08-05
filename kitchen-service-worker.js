93cb4e05558a46705a871edec2fe81d65210d474
musubi202607 <musubi.202607@gmail.com>
2026-08-05 11:01:51 +0900

    Enhance service worker caching logic

--- kitchen-service-worker.js
+++ kitchen-service-worker.js
@@ -120,17 +120,32 @@ event=>{
 event.respondWith(
 
 fetch(event.request)
+
 .then(response=>{
+
+const clone =
+response.clone();
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+
+cache.put(
+event.request,
+clone
+);
+
+});
 
 return response;
 
 })
+
 .catch(()=>{
 
-return caches.match(event.request);
+return caches.match(
+event.request
+);
 
 })
 
 );
-
-});
