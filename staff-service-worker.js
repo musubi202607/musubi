45f2d558feb41bd3cbbc6ac43594829734f6545e
musubi202607 <musubi.202607@gmail.com>
2026-08-11 15:00:18 +0900

    Update staff-service-worker.js

--- staff-service-worker.js
+++ staff-service-worker.js
@@ -1,101 +1,77 @@
-const CACHE_NAME = "musubi-staff-v2";
+const CACHE_NAME = "musubi-staff-v3";
 
 const FILES = [
-
   "staff.html",
-
   "staff-order.html",
   "staff-bbq.html",
   "bbq-option.html",
-
   "payment-waiting.html",
 
   "css/style.css",
 
   "js/config.js",
   "js/staff-auth.js",
-
   "js/staff-order.js",
   "js/bbq.js",
-  "js/bbq-tablet.js",
- 
+  "js/bbq-tablet.js"
 ];
 
+self.addEventListener("install", event => {
 
-self.addEventListener(
-  "install",
-  event => {
+  event.waitUntil(
 
-    event.waitUntil(
-
-      caches.open(CACHE_NAME)
-      .then(
-        cache =>
-          cache.addAll(FILES)
+    caches.open(CACHE_NAME)
+      .then(cache =>
+        cache.addAll(FILES)
       )
 
-    );
+  );
 
-  }
-);
+  self.skipWaiting();
 
+});
 
-self.addEventListener(
-  "fetch",
-  event => {
+self.addEventListener("activate", event => {
 
-    event.respondWith(
+  event.waitUntil(
 
-      caches.match(event.request)
-      .then(
-
-        cached =>
-
-          cached ||
-          fetch(event.request)
-
-      )
-
-    );
-
-  }
-);
-
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches.keys()
-      .then(
-
-        keys =>
+    caches.keys()
+      .then(keys =>
 
         Promise.all(
 
-          keys.map(
+          keys.map(key => {
 
-            key => {
+            if(key !== CACHE_NAME){
 
-              if(
-                key !== CACHE_NAME
-              ){
-
-                return caches.delete(key);
-
-              }
+              return caches.delete(key);
 
             }
 
-          )
+          })
 
         )
 
       )
 
-    );
+  );
 
-  }
-);
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(cached =>
+
+        cached ||
+        fetch(event.request)
+
+      )
+
+  );
+
+});
